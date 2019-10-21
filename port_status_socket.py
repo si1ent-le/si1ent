e4ac2d0163e2b1f54b73aafbea6fb04a533ac3d8
si1ent <56815669+si1ent-le@users.noreply.github.com>
2019-10-21 15:44:43 +0800

    Update and rename port_status.py to port_status_socket.py

--- port_status_socket.py
+++ port_status_socket.py
@@ -2,9 +2,7 @@
 # Author:si1ent
 # socket实现端口扫描
 # 端口号只能指定范围实现
-
 import socket
-
 ip = raw_input("Please Input IP:")
 def ip_port_status(ip, port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
