b887ff423776b6873a8195110632ccd8535fe5eb
si1ent-le <liyongle@si1ent-2.local>
2020-05-05 17:11:48 +0800

    #

--- ip_ban.sh
+++ ip_ban.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#
+#Author:si1ent
+#Date:5/5
+
 for i in ` netstat -an | grep -i ':80 ' | grep 'TIME_WAIT' | awk '{print $5}' | cut -d : -f 1 | sort | uniq -c | awk '{if($1 > 50) {print $2}}'`; do
   echo $i
   echo $i >> /tmp/banip
-#基础iptables配置好以防后面无法访问等,保证以上条件满足后才会执行后续添加
-#  /sbin/iptables -F
+
+# 基础iptables配置好以防后面无法访问等,保证以上条件满足后才会执行后续添加
+# /sbin/iptables -F
   /sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
   /sbin/iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 8888 -j ACCEPT
   /sbin/iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 3306 -j ACCEPT
-
 # 出现TIME_WAIT且超过指定条数会被添加到iptables
   /sbin/iptables -A INPUT -p tcp -j DROP -s $i
-
 done
