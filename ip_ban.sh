08dfad1281b9c7eca3aafdd3ed1fe14bdf9367ce
si1ent-le <liyongle@si1ent-2.local>
2020-05-07 10:27:56 +0800

    ip_ban

--- ip_ban.sh
+++ ip_ban.sh
@@ -9,20 +9,22 @@ for i in ` netstat -an | grep -i ':80 ' | grep 'TIME_WAIT' | awk '{print $5}' | 
 
 # 基础iptables配置好以防后面无法访问等,保证以上条件满足后才会执行后续添加
   /sbin/iptables -F
-  /sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  22 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  8888 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 8888 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  3306 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 3306 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  443 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  21 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 21 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  8080 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
-  /sbin/iptables -A INPUT -p tcp --dport 6379 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  6379 -j ACCEPT
   /sbin/iptables -A OUTPUT -p tcp --sport 6379 -j ACCEPT
+  /sbin/iptables -A INPUT -p tcp --dport  1433 -j ACCEPT
+  /sbin/iptables -A OUTPUT -p tcp --sport 1433 -j ACCEPT
 #  /sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 #  /sbin/iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
 # 出现TIME_WAIT且超过指定条数会被添加到iptables
