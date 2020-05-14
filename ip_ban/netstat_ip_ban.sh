1d07c5750015dd516ac4b6d50b5d91ab19dbde81
si1ent-le <liyongle@si1ent-2.local>
2020-05-14 20:11:55 +0800

    netsta_ip_ban

--- ip_ban/netstat_ip_ban.sh
+++ ip_ban/netstat_ip_ban.sh
@@ -4,4 +4,23 @@
 
 #通过netstat统计当前连接服务器IP以/16网段形式除输出
 
-netstat -ntu|awk '{print $5}'|cut -d: -f1 -s |cut -f1,2,3 -d'.'|sed 's/$/.0/'|sort|uniq -c|sort -nk1 -r | awk '{if($1 > 1) {print $2}}'
+for i in `netstat -ntu|awk '{print $5}'|cut -d: -f1 -s |cut -f1,2,3 -d'.'|sed 's/$/.0/'|sort|uniq -c|sort -nk1 -r | awk '{if($1 > 100) {print $2}}'`; do
+
+echo $ip_list;
+
+/sbin/iptables -F
+/sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --sport 22 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --sport 3306 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 6379 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --sport 6379 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --sport 8888 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
+
+#分别添加16、24网段进行封堵
+/sbin/iptables -A INPUT -p tcp --dport 80 -j DROP -s $i/16
+/sbin/iptables -A INPUT -p tcp --dport 80 -j DROP -s $i/24
+
+done
+
